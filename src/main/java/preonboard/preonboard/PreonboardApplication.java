be205d9b4fba5c48be140ba74e867d41ef54feca
suhoon <jobak4200nim@nate.com>
2023-08-12 23:43:57 +0900

    chore: Initial project setup

---
+++ src/main/java/preonboard/preonboard/PreonboardApplication.java
@@ -0,0 +1,13 @@
+package preonboard.preonboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreonboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreonboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/preonboard/preonboard/PreonboardApplicationTests.java
@@ -0,0 +1,13 @@
+package preonboard.preonboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreonboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
